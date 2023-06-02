39aa1ca0757110772fd28b0853b0c32a5c35db30
Chun-gu <chungu.yi92@gmail.com>
2023-06-02 17:41:58 +0900

    불필요한 console.log 제거 및 설명 주석 추가

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,16 @@
-import https from "https";
+// import https from "https";
 import fetch from "node-fetch";
 
 export async function handler(event) {
-  // console.log(event);
   const {
     path,
+    // 후에 authorization 기능 추가를 대비해 남겨둠
     headers: { authorization },
     httpMethod,
     body: requestBody,
     rawQuery,
   } = event;
-  console.log(
-    "==============================requestBody==========================",
-    requestBody
-  );
+
   if (httpMethod === "OPTIONS")
     return {
       statusCode: 200,
@@ -25,28 +22,33 @@ export async function handler(event) {
       },
     };
 
+  // 서버가 현재 http라서 https 주석 처리
   // const httpsAgent = new https.Agent({
   //   rejectUnauthorized: false,
   // });
-  console.log(`${process.env.API_URL}${path}?${rawQuery}`);
+
   try {
     let payload = {
       method: httpMethod,
       headers: { "content-type": "application/json" },
     };
+
+    // 메서드가 GET이나 HEAD가 아닐 때만 body 첨부
     if (httpMethod !== "GET" && httpMethod !== "HEAD")
       payload = { ...payload, body: requestBody };
+
     const response = await fetch(
       `${process.env.API_URL}${path}?${rawQuery}`,
       payload
     );
     const { status, ok, headers } = response;
+
     const resJson = await response.json();
-    // console.log(resJson)
     const body = JSON.stringify(resJson);
+
     headers["Access-Control-Allow-Origin"] = "*";
     headers["content-type"] = "application/json;charset=UTF-8";
-    // console.log(headers);
+
     return {
       statusCode: status,
       ok,
@@ -55,6 +57,7 @@ export async function handler(event) {
     };
   } catch (err) {
     console.log("============ERROR============", err);
+
     return {
       statusCode: 404,
       statusText: err.message,
